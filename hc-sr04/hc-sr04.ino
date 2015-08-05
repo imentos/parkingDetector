48b1001c21fb692f6af96ed52c58eb9d68e88e90
Ray Kuo <ray.kuo@sap.com>
2015-08-04 21:57:15 -0700

    first commit

---
+++ hc-sr04/hc-sr04.ino
@@ -0,0 +1,44 @@
+#define trigPin 3
+#define echoPin 2
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+}
+
+long microsecondsToInches(long microseconds)
+{
+// According to Parallax's datasheet for the PING))), there are
+// 73.746 microseconds per inch (i.e. sound travels at 1130 feet per
+// second). This gives the distance travelled by the ping, outbound
+// and return, so we divide by 2 to get the distance of the obstacle.
+// See: http://www.parallax.com/dl/docs/prod/acc/28015-PI...
+return microseconds / 74 / 2;
+}
+
+#define distanceToCar 5
+#define distanceIgnore 4
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int duration, distance;
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(1000);
+  digitalWrite(trigPin, LOW);
+
+  duration = pulseIn(echoPin, HIGH);
+  distance = microsecondsToInches(duration);
+  delay(500);
+  
+  if (distance < distanceIgnore) {
+    Serial.println("Ignored");
+    return;
+  }
+
+  Serial.println(distance);  
+  if (distance > distanceToCar) {
+    Serial.println("Car moved");    
+  }
+}
